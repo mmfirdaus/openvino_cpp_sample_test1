6afa0922b781c657d588600fcbb288da64f82436
mmfirdaus <mmf@megatm1x-mobl.gar.corp.intel.com>
2023-07-14 19:42:46 +0800

    cpp files added

---
+++ openvino_cpp_mysample_1.cpp
@@ -0,0 +1,38 @@
+﻿// openvino_cpp_mysample_1.cpp : Defines the entry point for the application.
+//
+
+#include "openvino_cpp_mysample_1.h"
+
+using namespace std;
+
+int PrintWelcome() {
+	cout << "Password Accepted" << endl;
+	cout << "-------------------------------------" << endl;
+	cout << "Welcome to OpenVINO Version 2023.0.0" << endl;
+	cout << "-------------------------------------" << endl;
+
+	return 0;
+}
+
+int main()
+{
+	int pwd;
+	string Name = "Megat Firdaus";
+	cout << "Hello " + Name << endl;
+
+	cout << "Enter Password" << endl;
+	cin >> pwd;
+	cout << endl;
+
+	if (pwd == 78) {
+		PrintWelcome();
+		slog::info << ov::get_openvino_version() << slog::endl;
+
+
+	}
+	else {
+		cout << " Wrong Password" << endl;
+	}
+
+	return 0;
+}
---
+++ openvino_cpp_mysample_1.h
@@ -0,0 +1,19 @@
+﻿// openvino_cpp_mysample_1.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <iterator>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "openvino/openvino.hpp"
+
+#include "samples/args_helper.hpp"
+#include "samples/common.hpp"
+#include "samples/classification_results.h"
+#include "samples/slog.hpp"
+#include "format_reader_ptr.h"
